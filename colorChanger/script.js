9298c2ceb326a71df0150780c763d57431bb5166
amanrawat130 <imdemario@gmail.com>
2025-06-17 11:17:04 +0530

    final updates

--- colorChanger/script.js
+++ colorChanger/script.js
@@ -1,26 +1,40 @@
 
-    document.getElementById("blue").addEventListener("click", function () {
-      document.body.style.backgroundColor = document.body.style.backgroundColor === 'blue' ? "" : "blue";
-    });
+    // document.getElementById("blue").addEventListener("click", function () {
+    //   document.body.style.backgroundColor = document.body.style.backgroundColor === 'blue' ? "" : "blue";
+    // });
 
-    document.getElementById("green").addEventListener("click", function () {
-      document.body.style.backgroundColor = document.body.style.backgroundColor === "green" ? "" : "green";
-    });
+    // document.getElementById("green").addEventListener("click", function () {
+    //   document.body.style.backgroundColor = document.body.style.backgroundColor === "green" ? "" : "green";
+    // });
 
-    document.getElementById("yellow").addEventListener("click", function () {
-      document.body.style.backgroundColor = document.body.style.backgroundColor === "yellow" ? "" : "yellow";
-    });
+    // document.getElementById("yellow").addEventListener("click", function () {
+    //   document.body.style.backgroundColor = document.body.style.backgroundColor === "yellow" ? "" : "yellow";
+    // });
 
-    document.getElementById("pink").addEventListener("click", function () {
-      document.body.style.backgroundColor = document.body.style.backgroundColor === "pink" ? "": "pink";
-    });
+    // document.getElementById("pink").addEventListener("click", function () {
+    //   document.body.style.backgroundColor = document.body.style.backgroundColor === "pink" ? "": "pink";
+    // });
 
-    document.getElementById("red").addEventListener("click", function () {
-      document.body.style.backgroundColor = document.body.style.backgroundColor === "red" ? "" : "red";
-    });
-    document.getElementById("violet").addEventListener("click", function () {
-      document.body.style.backgroundColor = document.body.style.backgroundColor === "violet" ? "" : "violet";
-    });
+    // document.getElementById("red").addEventListener("click", function () {
+    //   document.body.style.backgroundColor = document.body.style.backgroundColor === "red" ? "" : "red";
+    // });
+    // document.getElementById("violet").addEventListener("click", function () {
+    //   document.body.style.backgroundColor = document.body.style.backgroundColor === "violet" ? "" : "violet";
+    // });
+
+// <--- function ----> 
+
+    const container = document.querySelectorAll(".box");
+
+      container.forEach(function(button){
+          button.addEventListener("click", function(){
+          console.log(this.id)
+          document.body.style.backgroundColor = document.body.style.backgroundColor === this.id ? "": this.id;
+                                       
+          })
+      })
+
+
 
 
     document.getElementById("btn").addEventListener("click", function(){
